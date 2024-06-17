0f040516e09a5a6ee33940c67c5103e453d24ac8
chappellh1 <chappellh@sou.edu>
2024-06-17 12:02:49 -0700

    first attempt to get a window running, cannot find -lglfw3, simeple fix

---
+++ build/CMakeFiles/MyGameEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyGameEngine.dir/src/main.cpp.o"
+  "CMakeFiles/MyGameEngine.dir/src/main.cpp.o.d"
+  "MyGameEngine"
+  "MyGameEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyGameEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
